1520d1e8f9db1f6a0c51a9f221df1c445bfb8358
ALEX208z <anshumanayush208@gmail.com>
2023-03-09 11:54:49 +0530

    done done

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
   const addTodo = (title, desc) => {
     console.log("adding todo", title, desc);
     let sno;
-    if (todos.length == 0) {
+    if (todos.length === 0) {
       sno = 1;
     } else {
       sno = todos[todos.length - 1].sno + 1;
